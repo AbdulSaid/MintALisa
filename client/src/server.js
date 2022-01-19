78aa12080d6de6e2d1c98a99fd2ab29dd8ec3bc0
Abdullah Habes <abdulla096@hotmail.com>
2022-01-18 23:27:22 -0500

    add BE api routes and update the server

--- client/src/server.js
+++ client/src/server.js
@@ -6,7 +6,8 @@ const ENV = require("./environment");
 const app = require("./application")(ENV);
 const db = require("./db");
 
-
+const charactersRoutes = require("./routes/characters")(db);
+app.use("/", charactersRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
