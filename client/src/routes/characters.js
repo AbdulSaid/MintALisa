4dc3698de851e9188bc84168a290373daf55e4b4
Abdullah Habes <abdulla096@hotmail.com>
2022-01-20 19:56:20 -0500

    update the findy by Id  routes

--- client/src/routes/characters.js
+++ client/src/routes/characters.js
@@ -43,70 +43,24 @@ module.exports = function (db) {
   });
 
   characterRoutes.get("/characters/attributes/:id", function (req, res) {
-    console.log("dna -- --- ---", req.params.id)
-    db.query(`SELECT characters.dna         as dna,
-                     characters.name        as name,
-                     characters.description as description,
-                     hats.name              as hat,
-                     mouths.name            as mouth,
-                     backgrounds.name       as background,
-                     glasses.name           as glasses,
-                     accessories.name       as accessories
-              FROM character_attributes
-                       JOIN characters ON character_attributes.dna = characters.dna
-                       JOIN hats ON hat_id = hats.id
-                       JOIN mouths ON mouth_id = mouths.id
-                       JOIN backgrounds ON background_id = backgrounds.id
-                       JOIN glasses ON glasses_id = glasses.id
-                       JOIN accessories ON accessory_id = accessories.id
-              WHERE character_attributes.dna = '${req.params.id}'`
-    ).then(({rows: characters}) => {
-      res.json(characters);
-    });
+    charactersController.getAttributeById(req.params.id)
+      .then(({rows: characters}) => res.json(characters));
   });
 
   characterRoutes.get("/characters/:id", function (req, res) {
-    db.query(`SELECT characters.dna          as dna,
-                     characters.name         as name,
-                     characters.description  as description,
-                     image,
-                     price,
-                     collections.name        as collection_name,
-                     collections.description as collection_description,
-                     minted,
-                     inventory.quantity      as quantity
-              FROM characters
-                       JOIN collections ON collection_id = collections.id
-                       JOIN inventory ON characters.dna = inventory.dna
-              WHERE characters.dna = '${req.params.id}'`
-    ).then(({rows: characters}) => {
-      res.json(characters);
-    });
+    charactersController.getById(req.params.id)
+      .then(({rows: characters}) => res.json(characters));
   });
 
   characterRoutes.post("/characters/:id", function (req, res) {
 
-    if (!req.body.minted) {
-      res.status(400).json({error: 'invalid request: minted value not set in POST body'});
-      return;
-    }
-
-    if (!req.body.quantity) {
-      res.status(400).json({error: 'invalid request: quantity value not set in POST body'});
-      return;
-    }
-
-    db.query(`WITH character_update AS (UPDATE characters
-        SET minted = '${req.body.minted}'
-        WHERE dna = '${req.params.id}')
-              UPDATE inventory
-              SET quantity = '${req.body.quantity}'
-              WHERE dna = '${req.params.id}'
-              RETURNING *`
-    ).then(({rows: results}) => {
-      results.length === 0 ? res.send("Minting update was not successful.") :
-        res.send(`The character ${req.params.id} has been minted successfully.`);
-    });
+    charactersController.updateCharacterById(req).then(result => {
+      const {code, message} = result;
+      res.status(code).json(message);
+    }).catch(result => {
+      const {code, error} = result;
+      res.status(code).json(error)
+    })
   });
 
 
