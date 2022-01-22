d6fde252ab27bca4baaeb15e236e1a477cdb9cc2
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2022-01-22 17:33:43 -0500

    Add filter option to gallery

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -9,6 +9,9 @@ export default function Gallery() {
   const [occurance, setOccurance] = useState([]);
   const [sortOpen, setSortOpen] = useState(false);
   const [filterOpen, setFilterOpen] = useState(false);
+  const [minted, setMinted] = useState([]);
+  const [available, setAvailable] = useState([]);
+
 
   const urlForCharacters = `http://localhost:8080/characters/`;
   const urlForOccurence = `http://localhost:8080/characters/attributes/occurrence/`;
@@ -18,29 +21,73 @@ export default function Gallery() {
       (res) => {
         const api1 = res[0].data;
         const api2 = res[1].data;
-        console.log('api1', api1);
-        console.log('api2',api2);
         setCharacters(api1);
         setOccurance(api2);
-      }
+      },
     );
   }, []);
 
+  const countMintedLisas = () => {
+    const unavailable = [];
+    const availableTemp = [];
+    for (let val of characters) {
+      (val.minted) ? unavailable.push(val) : available.push(val);
+    }
+    setMinted(unavailable);
+    setAvailable(availableTemp);
+    console.log('minted:', minted)
+    console.log('available: ', available)
+    console.log('characters: ', characters)
+  }
 
-  const sortHandler = (val) => {
+  const sortHandler = val => {
     const charactersSort = [...characters];
+    const sortedOccurence = [...occurance];
+
     if (val === 'price-up') {
       return setCharacters(charactersSort.sort((firstEl, secondEl) => parseFloat(firstEl.price) - parseFloat(secondEl.price)));
     } else if (val === 'price-down') {
       return setCharacters(charactersSort.sort((firstEl, secondEl) => parseFloat(secondEl.price) - parseFloat(firstEl.price)));
+    } else if (val === 'rarity-up') {
+      // COME BACK TO THIS
+      const tempOccur = sortedOccurence.sort((firstEl, secondEl) => {
+        let sumFirstEl = firstEl.hat_occurance + firstEl.mouth_occurance + firstEl.background_occurance + firstEl.glasses_occurance + firstEl.accessories_occurance;
+        let sumSecondEl = secondEl.hat_occurance + secondEl.mouth_occurance + secondEl.background_occurance + secondEl.glasses_occurance + secondEl.accessories_occurance;
+        return parseFloat(sumSecondEl) - parseFloat(sumFirstEl);
+      });
+    } else if (val === 'rarity-down') {
+      setOccurance(sortedOccurence.sort((firstEl, secondEl) => {
+        let sumFirstEl = firstEl.hat_occurance + firstEl.mouth_occurance + firstEl.background_occurance + firstEl.glasses_occurance + firstEl.accessories_occurance;
+        let sumSecondEl = secondEl.hat_occurance + secondEl.mouth_occurance + secondEl.background_occurance + secondEl.glasses_occurance + secondEl.accessories_occurance;
+        return parseFloat(sumFirstEl) - parseFloat(sumSecondEl);
+      }));
     }
   }
 
-  
+  const filterHandler = val => {
+    const unavailable = [];
+    const availableTemp = [];
+    fetch('http://localhost:8080/characters/')
+    .then(res => res.json())
+    .then(json => {
+      for (let val of json) {
+        (val.minted) ? unavailable.push(val) : available.push(val);
+      }
+      setAvailable(availableTemp);
+      setMinted(unavailable);
+    });
+    if (val === 'available') {
+      return setCharacters(available);
+    } else if (val === 'unavailable') {
+      return setCharacters(minted);
+    }
+  }
+
+
   return (
     <div className="gallery">
       <header className="header">
-        <p>10/50 MINTED <img className='diamond' src='images/diamond.svg' alt='gem'/></p>
+        <p>{minted.length}/50 MINTED <img className='diamond' src='images/diamond.svg' alt='gem' /></p>
         <h1>Gallery</h1>
       </header>
 
@@ -51,13 +98,13 @@ export default function Gallery() {
             setFilterOpen(!filterOpen)
             e.stopPropagation();
             setSortOpen(false);
-          }}><FontAwesomeIcon className='filter-icon' icon={ faFilter } /> Filter</button>
+          }}><FontAwesomeIcon className='filter-icon' icon={faFilter} /> Filter</button>
 
         {filterOpen &&
-            <ul className='filter-list'>
-              <li className='filter-option'>Available</li>
-              <li className='filter-option'>Minted</li>
-            </ul>
+          <ul className='filter-list'>
+            <li className='filter-option' onClick={() => filterHandler('available')}>Available</li>
+            <li className='filter-option' onClick={() => filterHandler('unavailable')}>Minted</li>
+          </ul>
         }
 
         <button
@@ -66,26 +113,30 @@ export default function Gallery() {
             e.stopPropagation();
             setSortOpen(!sortOpen);
             setFilterOpen(false);
-          }}><FontAwesomeIcon className='sort-icon' icon={ faSort } /> Sort</button>
+          }}><FontAwesomeIcon className='sort-icon' icon={faSort} /> Sort</button>
 
         {sortOpen &&
           <ul className='sort-list'>
             <li className='sort-option' onClick={() => sortHandler('price-up')}>Price: low to high</li>
             <li className='sort-option' onClick={() => sortHandler('price-down')}>Price: high to low</li>
-            <li className='sort-option'>Rarity: low to high</li>
-            <li className='sort-option'>Rarity: high to low</li>
+            <li className='sort-option' onClick={() => sortHandler('rarity-up')}>Rarity: low to high</li>
+            <li className='sort-option' onClick={() => sortHandler('rarity-down')}>Rarity: high to low</li>
           </ul>
         }
       </section>
 
       <section className='gallery-display-container'>
-        {characters.map((char, index) => <SingleGalleryDisplay
+        {characters && characters.map((char, index) => <SingleGalleryDisplay
           key={index}
           id={char.dna}
           imgUrl={char.image}
           name={char.name}
           price={char.price}
-          />)}
+          minted={char.minted}
+        />)}
+      </section>
+      <section className='none-minted-container'>
+        {characters.length === 0 && <h2 className='none-minted-msg'>All Mona Lisas are available!</h2>}
       </section>
     </div>
   );
