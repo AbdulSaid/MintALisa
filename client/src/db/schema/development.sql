588533e60da7e583f383358f03e54901b39440d4
Abdullah Habes <abdulla096@hotmail.com>
2022-01-26 20:08:11 -0500

    fix the attribute names

--- client/src/db/schema/development.sql
+++ client/src/db/schema/development.sql
@@ -5,22 +5,22 @@ INSERT INTO collections (id, name, description)
 VALUES (2, 'regular edition', 'The monalisa regular edition.');
 
 INSERT INTO mouths (id, name, rarity, price)
-VALUES (1, 'Angela-mouth', 20, 0.001),
-       (2, 'Real-smile', 23, 0.001),
-       (3, 'Devious-smile-', 7, 0.0017),
+VALUES (1, 'AngelaMouth', 20, 0.001),
+       (2, 'RealSmile', 23, 0.001),
+       (3, 'DeviousSmile', 7, 0.0017),
        (4, 'Fangs', 13, 0.0015),
-       (5, 'Side-smile', 7, 0.0017),
+       (5, 'SideSmile', 7, 0.0017),
        (6, 'Underbite', 12, 0.0015),
-       (7, 'Tongue-out', 9, 0.0017),
-       (8, 'Open-smile', 9, 0.0017);
+       (7, 'TonguOut', 9, 0.0017),
+       (8, 'OpenSmile', 9, 0.0017);
 
 INSERT INTO backgrounds (id, name, rarity, price)
 VALUES (1, 'Montreal', 4, 0.0021),
-       (2, 'Blue-art', 9, 0.0017),
+       (2, 'BlueArt', 9, 0.0017),
        (3, 'Monet', 6, 0.0017),
        (4, 'Cloud', 20, 0.001),
-       (5, 'Pink-blue', 7, 0.0017),
-       (6, 'Rusted-metal', 16, 0.0015),
+       (5, 'PinkBlue', 7, 0.0017),
+       (6, 'RustedMetal', 16, 0.0015),
        (7, 'Palms', 20, 0.001),
        (8, 'Bed', 14, 0.0015),
        (9, 'Louvre', 4, 0.0021);
@@ -28,9 +28,9 @@ VALUES (1, 'Montreal', 4, 0.0021),
 INSERT INTO hats (id, name, rarity, price)
 VALUES (1, 'Cowboy', 18, 0.0015),
        (2, 'Toque', 16, 0.0015),
-       (3, 'Pretty-headband', 11, 0.0015),
+       (3, 'PrettyHeadband', 11, 0.0015),
        (4, 'Alien', 3, 0.0021),
-       (5, 'Backwards-cap', 23, 0.001),
+       (5, 'BackwardsCap', 23, 0.001),
        (6, 'Beret', 16, 0.0015),
        (7, 'Crown', 10, 0.0021),
        (8, 'Graduation', 10, 0.0017);
@@ -40,10 +40,10 @@ VALUES (1, 'Round', 14, 0.0015),
        (2, 'Monocle', 6, 0.0017),
        (3, 'Cartoon', 21, 0.001),
        (4, 'Goggles', 8, 0.0017),
-       (5, 'Gold-rim', 22, 0.001),
+       (5, 'GoldRim', 22, 0.001),
        (6, 'Flared', 6, 0.0017),
        (7, 'Aviator', 17, 0.0015),
-       (8, 'Thug-life', 6, 0.0017);
+       (8, 'ThugLife', 6, 0.0017);
 
 INSERT INTO accessories (id, name, rarity, price)
 VALUES (1, 'Reporter', 17, 0.0015),
@@ -51,6 +51,6 @@ VALUES (1, 'Reporter', 17, 0.0015),
        (3, 'Flowers', 23, 0.001),
        (4, 'Book', 3, 0.0021),
        (5, 'Tattoo', 8, 0.0017),
-       (6, 'Cozy-cat', 27, 0.001),
+       (6, 'CozyCat', 27, 0.001),
        (7, 'Wine', 16, 0.0015),
-       (8, 'Alien-sidekick', 2, 0.0021);
+       (8, 'AlienSidekick', 2, 0.0021);
